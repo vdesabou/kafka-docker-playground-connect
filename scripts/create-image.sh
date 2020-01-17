0ccbd8c7f397fad94e5c6de8d5a4cd7c3be315c3
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-01-17 14:40:18 +0100

    added retries as confluent-hub commands are failing randomly

--- scripts/create-image.sh
+++ scripts/create-image.sh
@@ -2,6 +2,40 @@
 
 set -e
 
+function log() {
+  YELLOW='\033[0;33m'
+  NC='\033[0m' # No Color
+  echo -e "$YELLOW$@$NC"
+}
+
+function logerror() {
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+  echo -e "$RED$@$NC"
+}
+
+function logwarn() {
+  PURPLE='\033[0;35m'
+  NC='\033[0m' # No Color
+  echo -e "$PURPLE$@$NC"
+}
+
+function retry() {
+  local n=1
+  local max=5
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        logwarn "Command failed. Attempt $n/$max:"
+      else
+        logerror "The command has failed after $n attempts."
+        return 1
+      fi
+    }
+  done
+}
+
 for version in $@
 do
   export TAG=${version}
@@ -12,7 +46,7 @@ do
     # 5.0.2 and 5.0.3 are not available on the hub
     export EXCEPTION_TAG=5.0.1
   fi
-  docker build --build-arg TAG=$TAG --build-arg EXCEPTION_TAG=$EXCEPTION_TAG -t vdesabou/kafka-docker-playground-connect:$TAG .
+  retry docker build --build-arg TAG=$TAG --build-arg EXCEPTION_TAG=$EXCEPTION_TAG -t vdesabou/kafka-docker-playground-connect:$TAG .
 
   docker push vdesabou/kafka-docker-playground-connect:$TAG
 done
