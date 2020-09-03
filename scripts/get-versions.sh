402a3ba2bcf419641c87aac0f56c1f150b39bd82
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-09-03 17:38:28 +0200

    test

--- scripts/get-versions.sh
+++ scripts/get-versions.sh
@@ -23,10 +23,19 @@ set -x
 if [ ! -z "$TRAVIS" ]
 then
      # running with travis
-    git config --global user.email "GH_EMAIL"
-    git config --global user.name "GH_USERNAME"
+    git config --global user.email "$GH_EMAIL"
+    git config --global user.name "$GH_USERNAME"
 fi
 
 git add $readme_file
 git commit --message "Updating with latest version (travis build $TRAVIS_BUILD_NUMBER)"
 git push -v https://$GH_TOKEN@github.com/vdesabou/kafka-docker-playground-connect.git master
+
+
+
+# git config user.email "${GIT_COMMITTER_EMAIL}"
+# git config user.name "${GIT_COMMITTER_NAME}"
+# git remote rm origin
+# git remote add origin https://${GIT_COMMITTER_ACCOUNT}:${GITHUB_API_TOKEN}@yourrepo.git > /dev/null 2>&1"
+# git commit -am 'Commit message'
+# git push origin HEAD:<yourbranch>
