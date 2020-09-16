e5c14b7563c16458abe065f868fb583404eefd35
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-09-16 14:56:08 +0200

    fix

--- scripts/launch-github-action.sh
+++ scripts/launch-github-action.sh
@@ -6,4 +6,4 @@ curl -H "Accept: application/vnd.github.everest-preview+json" \
     -H "Authorization: token $GH_TOKEN" \
     --request POST \
     --data '{"event_type": "Updating with latest version (travis build $TRAVIS_BUILD_NUMBER)"}' \
-    https://api.github.com/repos/vdesabou/kafka-docker-playground-connect/dispatches
+    https://api.github.com/repos/vdesabou/kafka-docker-playground/dispatches
