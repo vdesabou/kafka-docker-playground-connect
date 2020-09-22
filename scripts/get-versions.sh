e512fffd1a84d518dc223913c9d0048cfd59ac03
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2020-09-22 12:21:06 +0200

    update

--- scripts/get-versions.sh
+++ scripts/get-versions.sh
@@ -12,9 +12,14 @@ echo "| connector  | version |" >> $readme_tmp_file
 echo "|---|---|" >> $readme_tmp_file
 for dir in $(docker run vdesabou/kafka-docker-playground-connect:${image_version} ls /usr/share/confluent-hub-components/)
 do
-    version=$(docker run vdesabou/kafka-docker-playground-connect:${image_version} cat /usr/share/confluent-hub-components/${dir}/manifest.json | jq -r '.version')
+    if [ "$dir" = "kafka-connect-couchbase" ]
+    then
+        echo "| $dir  | 3.4.8 |" >> $readme_tmp_file
+    else
+        version=$(docker run vdesabou/kafka-docker-playground-connect:${image_version} cat /usr/share/confluent-hub-components/${dir}/manifest.json | jq -r '.version')
 
-    echo "| $dir  | $version |" >> $readme_tmp_file
+        echo "| $dir  | $version |" >> $readme_tmp_file
+    fi
 done
 
 cp $readme_tmp_file $readme_file
