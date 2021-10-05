6a71ed6f18b59e3460d3ee35af1a5b8fb2fd723c
Vincent de Saboulin <vdesabou@users.noreply.github.com>
2021-10-05 13:27:19 +0200

    Update create-image.sh

--- scripts/create-image.sh
+++ scripts/create-image.sh
@@ -62,7 +62,7 @@ do
   else
       export CP_CONNECT_IMAGE=cp-kafka-connect-base
   fi
-  export CP_CONNECT_IMAGE=cp-kafka-connect-base
+  
   TAG_JDBC="latest"
   if ! version_gt $TAG_BASE "5.9.0"; then
     # for version less than 6.0.0, use JDBC I' with same version
