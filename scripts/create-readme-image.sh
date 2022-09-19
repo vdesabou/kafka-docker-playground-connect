6a5a974de61e39098472251cded633db65d035f7
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2022-09-19 11:26:18 +0200

    fix

--- scripts/create-readme-image.sh
+++ scripts/create-readme-image.sh
@@ -39,42 +39,30 @@ function retry() {
 # https://stackoverflow.com/a/24067243
 function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
 
-
-function docker_tag_exists() {
-    curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
-}
-
 for version in $@
 do
-  if docker_tag_exists confluentinc/cp-server-connect-base "${version}"
+  export TAG=${version}
+  # to handle ubi8 images
+  export TAG_BASE=$(echo $TAG | cut -d "-" -f1)
+
+  if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
   then
-    export TAG=${version}
-    # to handle ubi8 images
-    export TAG_BASE=$(echo $TAG | cut -d "-" -f1)
-
-    if [[ "$TAG" == *ubi8 ]] || version_gt $TAG_BASE "5.9.0"
-    then
-      CONNECT_USER="appuser"
-    else
-      CONNECT_USER="root"
-    fi
-
-    TAG_JDBC="latest"
-    if ! version_gt $TAG_BASE "5.9.0"; then
-      # for version less than 6.0.0, use JDBC with same version
-      # see https://github.com/vdesabou/kafka-docker-playground/issues/221
-      TAG_JDBC=$TAG_BASE
-    fi
-
-    if [ "$TAG_BASE" = "5.0.2" ] || [ "$TAG_BASE" = "5.0.3" ]
-    then
-      TAG_JDBC="5.0.1"
-    fi
-
-    retry docker build -f Dockerfile-readme-image --build-arg TAG=$TAG --build-arg TAG_BASE=$TAG_BASE --build-arg TAG_JDBC=$TAG_JDBC --build-arg CONNECT_USER=$CONNECT_USER -t vdesabou/kafka-docker-playground-connect-readme:$TAG .
+    CONNECT_USER="appuser"
   else
-    logwarn "confluentinc/cp-server-connect-base ${version} does not exist"
-    continue
+    CONNECT_USER="root"
   fi
 
+  TAG_JDBC="latest"
+  if ! version_gt $TAG_BASE "5.9.0"; then
+    # for version less than 6.0.0, use JDBC with same version
+    # see https://github.com/vdesabou/kafka-docker-playground/issues/221
+    TAG_JDBC=$TAG_BASE
+  fi
+
+  if [ "$TAG_BASE" = "5.0.2" ] || [ "$TAG_BASE" = "5.0.3" ]
+  then
+    TAG_JDBC="5.0.1"
+  fi
+
+  retry docker build -f Dockerfile-readme-image --build-arg TAG=$TAG --build-arg TAG_BASE=$TAG_BASE --build-arg TAG_JDBC=$TAG_JDBC --build-arg CONNECT_USER=$CONNECT_USER -t vdesabou/kafka-docker-playground-connect-readme:$TAG .
 done
